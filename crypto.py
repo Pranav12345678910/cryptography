93a10f878da7db59ceb5f45c9703f02d2936ba44
Pranav <pstatavarti@gmail.com>
2020-09-23 16:42:54 -0700

    Vigenere encryption

--- crypto.py
+++ crypto.py
@@ -10,7 +10,6 @@ def encrypt_caesar(plaintext, offset):
             #at this point value_int represents the remainder if there is one, so you need to add it to the starting point which is A, because it doesnt start at 0
             value_int = value_int - ord('Z') - 1 + ord('A')
         final_string += chr(value_int)
-        print(final_string) 
     return final_string 
     
 
@@ -30,12 +29,41 @@ def decrypt_caesar(ciphertext, offset):
 # Arguments: string, string
 # Returns: string
 def encrypt_vigenere(plaintext, keyword):
-    pass
+    #string that represents the length adjusted key
+    encrypted_result = ""
+    key = ""
+    if len(keyword) < len(plaintext):
+        repeats = len(plaintext) // len(keyword)
+        key = keyword * repeats
+        #in case the length of the plaintext is not a multiple of the length of the keyword
+        if len(key) != len(plaintext):
+            difference = len(plaintext) - len(key)
+            #adding the part that is remaining
+            key += keyword[0:difference]
+    if len(keyword) > len(plaintext):
+        difference = len(keyword) - len(plaintext)
+        key += keyword[0:difference - 1]
+    if len(keyword) == len(plaintext):
+        key = keyword   
+    x = 0
+    while x < len(plaintext) and x < len(key):
+        #number that corresponds to encrypted character
+        char_value = ord(plaintext[x])
+        key_char_value = ord(key[x]) - ord('A')
+        char_value += key_char_value
+        if char_value > ord('Z'):
+            char_value = char_value - ord('Z') - 1 + ord('A') 
+        encrypted_result += chr(char_value)      
+        x += 1 
+    return encrypted_result
+        
+    
+    #return final_string
 
 # Arguments: string, string
 # Returns: string
 def decrypt_vigenere(ciphertext, keyword):
-    pass
+    
 
 # Merkle-Hellman Knapsack Cryptosystem
 # Arguments: integer
@@ -59,7 +87,7 @@ def decrypt_mhkc(ciphertext, private_key):
     pass
 
 def main(): 
-    print(decrypt_caesar("ERE", 3))
+    print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
 
 if __name__ == "__main__":
     main()
